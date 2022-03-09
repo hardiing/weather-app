5bfeeb07f7440b6cd3e24eac48ef4ef18979a514
Nathan Harding <nathanharding02@gmail.com>
2022-03-09 16:19:46 -0700

    Added the rest of the fields, header, some styling, and started gif step

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const searchBtn = document.getElementById("searchBtn");
 const cityName = document.getElementById("cityName");
+const temperature = document.getElementById("temperature");
+const feelsLike = document.getElementById("feelsLike");
+const humidity = document.getElementById("humidity");
+const windSpeed = document.getElementById("wind");
 
 searchBtn.addEventListener("click", async () => {
     const location = document.getElementById("search").value;
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=aaac8290bc8f6b69e94cd98f1491fee6`, {mode: "cors"});
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&APPID=aaac8290bc8f6b69e94cd98f1491fee6`, {mode: "cors"});
     const searchResults = convertData(await response.json());
     cityName.textContent = `${searchResults.cityName}`;
+    temperature.textContent = `Temperature: ${searchResults.temperature}° F`;
+    feelsLike.textContent = `Feels Like: ${searchResults.feelsLike}° F`;
+    humidity.textContent = `Humidity: ${searchResults.humidity}%`;
+    windSpeed.textContent = `Wind Speed: ${searchResults.windSpeed} mph`;
     return searchResults;
 });
 
